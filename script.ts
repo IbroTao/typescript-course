1ca53aea5252a0bab78fb51d81c0768f0660ecd2
IbroTao <ibrahimtaofeek005@gmail.com>
2024-01-14 13:12:48 +0100

    Assign types: Number, String, Boolean & Any

--- script.ts
+++ script.ts
@@ -1,3 +1,16 @@
 // String
 let myName = "Tao";
-//myName = 49;
+// myName = 49;
+
+// Number
+let myAge = 67;
+// myAge = "Alexia"
+
+// Boolean
+let hasHobbies = true;
+// hasHobbies = 6
+
+//Types
+let myRealAge: number;
+myRealAge = 27;
+//myRealAge = '27';
